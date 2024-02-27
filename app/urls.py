55f279dc76cdbf50cfe8dd7b5662f516a91f79d7
tiago-ntt <tiago.reissilva@nttdata.com>
2024-02-27 20:15:38 -0300

    add routers save and get by id

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from django.http import JsonResponse
+from genres.views import genre_create_list_view, genre_detail_view
 
-def hello_view(resquest):
-    return JsonResponse({'message': 'Vamos ver'})
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('hello/', hello_view)
+    path("admin/", admin.site.urls),
+    path("genres/", genre_create_list_view, name="genre_create_list_view"),
+    path("genres/<int:pk>/", genre_detail_view, name="genre_detail_view"),
 ]
